0cb75e890fffbefc8d121ea2eceeac70fe97c589
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-10-26 21:47:13 +0300

    feat: add checking of existing of user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,25 @@
 
 import Launcher from '@/app/dashboard/components/Launcher';
 import { useRouter } from 'next/navigation';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase';
 
 export default function Home() {
+  const [user] = useAuthState(auth);
   const { push } = useRouter();
   setTimeout(() => push('/auth'), 1000);
+
+  console.log(user);
+
+  const checkUser = () => {
+    if (!user) {
+      return;
+    } else if (user == null) {
+      push('/auth');
+    } else {
+      push('/dashboard');
+    }
+  };
 
   return (
     <main>
